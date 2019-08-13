d0d1a59941dd50daf811543dee808ee6d4c4e2b7
yaoyu bao <yaobao5354@gmail.com>
2019-08-13 05:40:46 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -16,6 +16,6 @@ def move(board, index, value = "X")
   def update_array_at_with(array, index, value)
   array[index] = value
   end 
-update_array_at_with(board, 0, "X")
+update_array_at_with(board, index, value)
 
 end
